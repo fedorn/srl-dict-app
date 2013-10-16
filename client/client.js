67e15f31fe0049a072e421a6f7645f55343fc79c
fsqcds <fsqcds@gmail.com>
2013-10-16 19:27:58 +0400

    verb args editing

--- client/client.js
+++ client/client.js
@@ -6,6 +6,8 @@ var currentTypeArgs = function() {
   type = currentType();
   if (type) {
     return TypeArgs.find({type_id: type._id});
+  } else {
+    return [];
   }
 }
 
@@ -24,7 +26,7 @@ Meteor.startup(function () {
 // Returns an event map that handles the "escape" and "return" keys and
 // "blur" events on a text input (given by selector) and interprets them
 // as "ok" or "cancel".
-var okCancelEvents = function (selector, callbacks) {
+var okCancelEvents = function (selector, callbacks, canBeEmpty) {
   var ok = callbacks.ok || function () {};
   var cancel = callbacks.cancel || function () {};
 
@@ -39,7 +41,7 @@ var okCancelEvents = function (selector, callbacks) {
                  evt.type === "focusout") {
         // blur/return/enter = ok/submit if non-empty
         var value = String(evt.target.value || "");
-        if (value)
+        if (value || canBeEmpty)
           ok.call(this, value, evt);
         else
           cancel.call(this, evt);
@@ -66,7 +68,7 @@ Template.types.active = function () {
   return Session.equals("type_name", this.name) ? 'active' : ''
 }
 
-Template.args.args = function () {
+Template.type_args.type_args = function () {
   return currentTypeArgs();
 }
 
@@ -86,7 +88,9 @@ Template.verbs.events(okCancelEvents(
       });
       evt.target.value = '';
     }
-  }));
+  },
+  false
+));
 
 Template.verbs.verbs = function () {
   var type = currentType();
@@ -96,10 +100,68 @@ Template.verbs.verbs = function () {
   return Verbs.find({type_id: type._id}, {sort: {inf: 1}});
 };
 
-Template.verbEdit.args = function () {
-  return currentTypeArgs();
+Template.verb_edit.args = function () {
+  var verb_id = this._id;
+  return _.map(currentTypeArgs().fetch(), function(type_arg) {
+    var selector = {type_id: type_arg._id, verb_id: verb_id};
+    var arg = Args.findOne(selector);
+    return {
+      name: type_arg.name,
+      prep: arg ? arg.prep : '',
+      noun_case: arg ? arg.noun_case : '',
+      selector: selector,
+      arg: function() {
+        return arg = Args.findOne(selector);
+      }
+    };
+  });
 }
 
+Template.arg.noun_cases = function () {
+  return NounCases;
+}
+
+Template.arg.selected_case = function(value) {
+  return (value == this ? ' selected' : '');
+}
+
+Template.arg.events(okCancelEvents(
+  '.prep-input',
+  {
+    ok: function(value) {
+      var arg = this.arg();
+      if (value) {
+        if (arg) {
+          Args.update({_id: arg._id}, {$set: {prep: value}});
+        } else {
+          Args.insert(_.extend(this.selector, {prep: value}));
+        }
+      } else if (arg) {
+        Args.update({_id: arg._id}, {$unset: {prep: value}})
+      }
+    }
+  },
+  true
+));
+
+Template.arg.events({
+  'change .noun-case-select': function(evt) {
+    var value = evt.target.value;
+    var arg = this.arg();
+    if (value) {
+      if (arg) {
+        console.log("if value if arg");
+        Args.update({_id: arg._id}, {$set: {noun_case: value}});
+      } else {
+        console.log("if value else");
+        Args.insert(_.extend(this.selector, {noun_case: value}));
+      }
+    } else if (arg) {
+      console.log("else if arg");
+      Args.update({_id: arg._id}, {$unset: {noun_case: value}})
+    }
+  }
+});
 
 // Track type in URL
 var SrlRouter = Backbone.Router.extend({
--- model.js
+++ model.js
@@ -2,3 +2,4 @@ Types = new Meteor.Collection("types")
 TypeArgs = new Meteor.Collection("typeArgs")
 Verbs = new Meteor.Collection("verbs")
 Args = new Meteor.Collection("args")
+NounCases = ["nomn", "gent", "datv", "accs", "ablt", "loct"]
