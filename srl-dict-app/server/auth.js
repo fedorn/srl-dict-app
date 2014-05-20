1f7d19e6749b901e13a4a61cd369c66ecac16e55
fsqcds <fsqcds@gmail.com>
2014-05-20 21:41:40 +0400

    Don't require auth on /data.json

--- srl-dict-app/server/auth.js
+++ srl-dict-app/server/auth.js
@@ -1,5 +1,8 @@
 Meteor.startup(function () {
   WebApp.connectHandlers.stack.splice(0, 0, {
+    route: '/data.json',
+    handle: WebApp.connectHandlers.stack[0].handle
+  }, {
     route: '',
     handle: WebApp.__basicAuth__(function(user, pass){
       return Meteor.settings.user == user & Meteor.settings.password == pass;
