f58f40ae8fd9f93a2d2225eb32068b1a9bd10f92
fsqcds <fsqcds@gmail.com>
2014-05-20 20:44:15 +0400

    Return json with all data on /data.json

---
+++ srl-dict-app/server/json.js
@@ -0,0 +1,35 @@
+Meteor.startup(function () {
+  HTTP.methods({
+    'data.json': function() {
+      var dataFromArg = function(arg) {
+        var dataArg = {}
+        if (arg.prep) {
+          dataArg.prep = arg.prep;
+        }
+        if (arg.noun_case) {
+          dataArg.noun_case = arg.noun_case;
+        }
+        if (arg.example) {
+          dataArg.example = arg.example;
+        }
+        if (arg.type_arg_id) {
+          dataArg.type_arg = TypeArgs.find({_id: arg.type_arg_id}).fetch()[0].name;
+        }
+        return dataArg;
+      };
+
+      this.setContentType('application/json');
+      var data = {};
+
+      Args.find().forEach(function(arg) {
+        var verb = Verbs.find({_id: arg.verb_id}).fetch()[0];
+        var type = Types.find({_id: verb.type_id}).fetch()[0];
+        if (!data[type.name]) { data[type.name] = {}; }
+        if (!data[type.name][verb.inf]) { data[type.name][verb.inf] = []; }
+        var dataArg = dataFromArg(arg);
+        data[type.name][verb.inf].push(dataArg);
+      });
+      return EJSON.stringify(data);
+    }
+  });
+});
